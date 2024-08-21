b36b6e9d86bf08d1bbf11cb140ecd5148811890d
gabriella_jacubavicius@hotmail.com <gabriella_jacubavicius@hotmail.com>
2024-08-21 16:36:01 -0300

    fix: ajustes nas configurações duplicadas

--- src/utils/toastAlerta.ts
+++ src/utils/toastAlerta.ts
@@ -1,57 +1,65 @@
 import { toast } from 'react-toastify';
 
 export function toastAlerta(mensagem: string, tipo: string) {
-  switch (tipo) {
-    case 'sucesso':
-      toast.success(mensagem, {
-        position: 'top-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: 'colored',
-        progress: undefined,
-      });
-      break;
+  const toastId = mensagem;
 
-    case 'info':
-      toast.info(mensagem, {
-        position: 'top-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: 'colored',
-        progress: undefined,
-      });
-      break;
+  if (!toast.isActive(toastId)) {
+    switch (tipo) {
+      case 'sucesso':
+        toast.success(mensagem, {
+          toastId,
+          position: 'top-right',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: false,
+          theme: 'colored',
+          progress: undefined,
+        });
+        break;
 
-    case 'erro':
-      toast.error(mensagem, {
-        position: 'top-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: 'colored',
-        progress: undefined,
-      });
-      break;
+      case 'info':
+        toast.info(mensagem, {
+          toastId,
+          position: 'top-right',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: false,
+          theme: 'colored',
+          progress: undefined,
+        });
+        break;
 
-    default:
-      toast.info(mensagem, {
-        position: 'top-right',
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        theme: 'colored',
-        progress: undefined,
-      });
-      break;
+      case 'erro':
+        toast.error(mensagem, {
+          toastId,
+          position: 'top-right',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: false,
+          theme: 'colored',
+          progress: undefined,
+        });
+        break;
+
+      default:
+        toast.info(mensagem, {
+          toastId,
+          position: 'top-right',
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: false,
+          theme: 'colored',
+          progress: undefined,
+        });
+        break;
+    }
   }
 }
